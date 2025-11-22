2a504217ef33b3156039bc8a59917ba6b6c13286
Manav Solanki <manavsolanki344@gmail.com>
2025-11-22 22:40:28 +0530

    Fix App.jsx routing and set Home as default page; corrected imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Header from "../src/common-componets/Header";
-import Footer from "../src/common-componets/Footer";
-import Home from "./componets/Home"
-import About from "./componets/About"
+import Header from "./common-componets/Header";
+import Footer from "./common-componets/Footer";
+import Home from "./componets/Home";
+import About from "./componets/About";
 import Packages from "./componets/Packages";
 import Destinations from "./componets/Destinations";
 import Guide from "./componets/Guide";
 import Gallery from "./componets/Gallery";
 import Contact from "./componets/Contact";
 
-
+// Layout with Header and Footer
 const Layout = () => {
   return (
     <div>
@@ -20,62 +20,46 @@ const Layout = () => {
   );
 };
 
-
-
-
 function App() {
   const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { 
-        path: "/home", 
-        element: <Home/>
-      },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
 
-      {
-        path: "/destinations",
-        element: <Destinations/>
-      },
-      
+        },
+        {
+          path: "/destinations",
+          element: <Destinations />
+        },
+        {
+          path: "/travelpackages",
+          element: <Packages />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/guide",
+          element: <Guide />
+        },
+        {
+          path: "/gallery",
+          element: <Gallery />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+      ],
+    },
+  ]);
 
-      {
-        path: "/travelpackages",
-        element: <Packages/>
-      },
-        
-      
-
-      { 
-        path: "/about", 
-        element: <About /> 
-      },
-
-      {
-        path:"/guide",
-        element:<Guide/>
-      },
-
-      {
-        path:"/gallery",
-        element:<Gallery/>
-      },
-
-      {
-        path:"/contact",
-        element:<Contact/>
-      },
-
-      
-    ],
-  },
-]);
-  return (
-    <>
-    <RouterProvider router={router} />;
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
